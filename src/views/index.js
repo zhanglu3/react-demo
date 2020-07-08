cc39d3e90a9db61897c45c6e665d391efbcb5578
zhanglulu <zhanglulu@souche.com>
2020-07-08 14:24:33 +0800

    feat: dev

--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
     Route,
     Link,
@@ -7,12 +7,13 @@ import {
     useParams,
     useRouteMatch,
     useHistory,
+    useLocation,
 } from "react-router-dom";
 
 import LazyLoad from "../router/lazyload";
 
 function Child(props) {
-    console.log(props)
+    console.log('ChildTest', props)
     let { page} = useParams();
     let num=props.match.params.num;
     return (
@@ -26,24 +27,23 @@ function Child(props) {
 
 export default function() {
     let { path, url } = useRouteMatch();
-    let isLogin = true;
+    let [isLogin, setIsLogin] = useState(false);
     let history = useHistory();
 
-    const LoginBtn = () =>  <button onClick={()=>{isLogin = true;history.push(`${url}/protected`)}}>登录</button>
-
-    const LoginOutBtn = () => {
-    return <button onClick={()=>{isLogin = false;history.push(`${url}/login`)}}>退出登录</button>
-    }
-
+    const LoginBtn = () =>  <button onClick={()=>{setIsLogin(true); history.push(`${url}/protected`)}}>登录</button>
     const Login = () => <LoginBtn/>
-
+    const LoginOutBtn = () => {
+        return <button onClick={()=>{setIsLogin(false); history.push(`${url}/login`)}}>退出登录</button>
+    }
     const Condition = ({ children }) => isLogin ? children : <Redirect to={`${url}/login`} />
-
     const PrivateRoute = ({ children, ...rest }) => {
         return (
             <Route {...rest} render={() => <Condition children={children} />} />
         );
     }
+
+    let [firstTest, setFirstTest] = useState(0);
+    let location = useLocation();
 
     return (
         <Fragment>
@@ -55,7 +55,16 @@ export default function() {
                     <li><Link to={`${url}/test1/number1`}>Test1-1</Link></li>
                     <li><Link to={`${url}/test2/number2`}>Test2-2</Link></li>
                     <li><Link to={`${url}/test3/number3`}>Test3-3</Link></li>
-                    <li><Link to={`${url}/protected`}>protected</Link></li>
+                    <li><Link to={`${url}/protected`}>Auth</Link></li>
+                    <li>
+                        <Link to={`${url}/1?testVal=${firstTest}`}
+                              onClick={() => {
+                                  console.log('Link 标签（跳转到/1）的 onClick 事件', location)
+                              }}>
+                            示例1
+                        </Link>
+                        <input type="text" value={firstTest} onChange={(e) => setFirstTest(e.target.value)}/>
+                    </li>
                 </ul>
                 <Switch>
                     <Route exact path={`${path}/game`} component={LazyLoad(()=>import("./game"))}></Route>
@@ -66,6 +75,11 @@ export default function() {
                         <h2>已经登录 可查看-protected</h2>
                         <LoginOutBtn/>
                     </PrivateRoute>
+                    <Route path={`${path}/1`} component={(props) => (
+                        <div>
+                            第一个组件读取参数（location.search） {props.location.search}{console.log('示例1', props)}
+                        </div>
+                    )}/>
                 </Switch>
             </div>
         </Fragment>
